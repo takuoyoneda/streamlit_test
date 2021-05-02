091e432c206fca4859b1e26d4914c950fcdbfdbb
root <shakeweb@gmail.com>
2021-05-02 20:01:52 +0900

    update

--- test.py
+++ test.py
@@ -9,14 +9,6 @@ st.write(df)
 st.dataframe(df.style.highlight_max(axis=0), width=200, height=200)
 st.table(df.style.highlight_max(axis=0))
 
-
-
-
-st.write('Charts')
-df = pd.DataFrame(
-    np.random.rand(20,3),
-    columns=['a','b','c']
-)
 st.line_chart(df)
 st.area_chart(df)
 st.bar_chart(df)
